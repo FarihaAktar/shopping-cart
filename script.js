73708e09bdd99f5bdbab6120deaf0cb4512e72b3
Farihaakter <yellowfariha@gmail.com>
2021-01-28 15:51:35 +0600

    added check out handler

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// iphone11 "+" button handler
+// iphone11  button handler
 const plusButton = document.getElementById("plus-btn");
 plusButton.addEventListener('click', function(){
-    // const numberOfPhone = document.getElementById("phone-quantity").value;
-    // const parseNumber = parseFloat(numberOfPhone);
-    // const totalNumberOfPhone = parseNumber + 1;
-    // document.getElementById("phone-quantity").value = totalNumberOfPhone;
     getInputNumber("phone-quantity", 1);
     updateSpanText("phone-budget", 1219);
-
-    // const phoneBudget = document.getElementById("phone-budget").innerHTML;
-    // const phoneBudgetNumber = parseFloat(phoneBudget);
-    // const totalPhoneBudget = phoneBudgetNumber + 1219;
-    // document.getElementById("phone-budget").innerHTML = totalPhoneBudget;
 })
 const minusButton = document.getElementById("minus-btn");
 minusButton.addEventListener('click', function(){
@@ -19,6 +10,7 @@ minusButton.addEventListener('click', function(){
     updateSpanText("phone-budget", -1 * 1219);
 
 })
+// iphone11 phone case button handler
 const phoneCasePlusBtn = document.getElementById("plus-btn-ofPhoneCase");
 phoneCasePlusBtn.addEventListener('click', function(){
     getInputNumber("phoneCase-quantity", 1);
@@ -29,8 +21,18 @@ phoneCaseMinusBtn.addEventListener('click', function(){
     getInputNumber("phoneCase-quantity", -1 * 1);
     updateSpanText("phoneCase-budget", -1 * 59);
 })
+// check out button handler
+const checkOutBtn = document.getElementById("check-out-btn");
+checkOutBtn.addEventListener('click', function(){
+    const phoneBudget = parseFloat(document.getElementById("phone-budget").innerHTML);
+    const phoneCaseBudget = parseFloat(document.getElementById("phoneCase-budget").innerHTML);
+    const subTotal = phoneBudget + phoneCaseBudget;
+    document.getElementById("sub-total").innerHTML = subTotal;
+    document.getElementById("total").innerHTML = subTotal;
+    
+})
 
-// definition of function
+// definition of functions
 function getInputNumber(id, num){
     const amount = document.getElementById(id).value;
     const parseNumber = parseFloat(amount);
